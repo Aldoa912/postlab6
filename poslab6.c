1349db9d32f4eceecf838aba6fb05639241ff021
Aldoa912 <avi21257@uvg.edu.gt>
2022-11-07 19:05:25 -0600

    funcional

--- poslab6.c
+++ poslab6.c
@@ -78,11 +78,15 @@ void main(void) {
                     TXREG = ADC;
                     __delay_ms(100);
                     TXREG = 10;
-                    __delay_ms(100);
+                    
                     TXREG = 13;
                     flag = 1;
                 }
                 if (RCREG == '2'){
+                    __delay_ms(300);
+                    while(PIR1bits.RCIF == 0){
+                    ;
+                    }
                     if(PIR1bits.RCIF == 1){
                     PORTD = RCREG;
                     PIR1bits.RCIF = 0;
@@ -97,10 +101,10 @@ void main(void) {
         if(index>57)
             index=0;
         flag = 0;
-        if(PIR1bits.RCIF == 1){
-        PORTD = RCREG;
-        PIR1bits.RCIF = 0;
-        }
+//        if(PIR1bits.RCIF == 1){
+//        PORTD = RCREG;
+//        PIR1bits.RCIF = 0;
+//        }
 
     }
     return;
